4950aa2390f6bad2ead345e52a3a3c736d9fa8a0
益川良藏 <WASACON@masukawaryouzou-no-MacBook-Pro.local>
2016-07-06 20:25:46 +0900

    add all

---
+++ app/src/main/java/com/example/wasacon/draw_droid/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.wasacon.draw_droid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    //object oreented
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //TODO コードを書く
+    }
+}
